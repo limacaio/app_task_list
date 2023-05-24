7ad0548eeededad484eae1e9304202290262bba2
Caio Lima <caioifg@gmail.com>
2023-05-24 13:59:58 -0300

    set orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:app_task_list/pages/todo_list_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) => runApp(
+      const MyApp(),
+    ),
+  );
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -15,9 +21,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TodoListPage()
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: TodoListPage());
   }
 }
